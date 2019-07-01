8ac6c939122912bd5913f5125fc6b85308c2437f
Rob Sharratt <robsharratt@ticklemyfish.co.uk>
2019-07-02 00:05:23 +0100

    Initial check-in of module TCGCollector

---
+++ TCGCollector/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TCGCollector.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
