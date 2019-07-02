81606620a76e064d677f1ff3adb0a5ed0d4fdbd5
Rob Sharratt <robsharratt@ticklemyfish.co.uk>
2019-07-02 23:51:10 +0100

    CardCatsController

---
+++ TCGCollector/Controllers/CardCatsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TCGCollector.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TCGCollector.Controllers
+{
+    public class CardCatsController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var entities = new CardCatDBEntities();
+
+            return View(entities.MovieSet.ToList());
+            //return View();
+        }
+
+        //[HttpGet]
+        //public ActionResult<IEnumerable<CardCat>> Get()
+        //{
+        //    return _repository.GetProducts();
+        //}
+    }
+}
---
+++ TCGCollector/Models/ViewModels/CardCatsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using TCGCollector.Models;
+
+namespace TCGCollector.Models.ViewModels
+{
+    public class CardCatsViewModel
+    {
+        public IEnumerable<CardCat> CardCats { get; set; }
+        //public PagingInfo PagingInfo { get; set; }
+        //public string CurrentCategory { get; set; }
+    }
+}
