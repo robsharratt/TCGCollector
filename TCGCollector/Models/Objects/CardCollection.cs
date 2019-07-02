eab809ab8eb3c55ea5ff18b2e622e0b5b01a65ed
Rob Sharratt <robsharratt@ticklemyfish.co.uk>
2019-07-02 22:46:54 +0100

    Added CardCollection

---
+++ TCGCollector/Models/Objects/CardCollection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+namespace TCGCollector.Models
+{
+    public class CardCollection
+    {
+        public int CardCollectionID { get; set; }
+        public string CardCollectionName { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedDate { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime LastUpdateDate { get; set; }
+        public ICollection<UserCardCollection> UserCardCollections { get; set; }
+    }
+}
